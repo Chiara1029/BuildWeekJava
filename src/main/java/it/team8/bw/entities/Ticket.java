eb4292827177f8bb7e00467bb1738553298acb8d
Chiara Puleio <chiarapuleio@yahoo.it>
2024-01-29 15:22:22 +0100

    children class: Ticket

--- src/main/java/it/team8/bw/entities/Ticket.java
+++ src/main/java/it/team8/bw/entities/Ticket.java
@@ -1,4 +1,39 @@
 package it.team8.bw.entities;
 
-public class Ticket {
+import it.team8.bw.abstractClass.TicketOffice;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="ticket")
+public class Ticket extends TicketOffice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private boolean obliterated;
+    private LocalDate convalidationDate;
+
+    public Ticket(){}
+
+    public Ticket(LocalDate emissionDate) {
+        super(emissionDate);
+        this.obliterated = false;
+    }
+
+    public boolean isObliterated() {
+        return obliterated;
+    }
+
+    public void setObliterated(boolean obliterated) {
+        this.obliterated = obliterated;
+    }
+
+    public LocalDate getConvalidationDate() {
+        return convalidationDate;
+    }
+
+    public void setConvalidationDate(LocalDate convalidationDate) {
+        this.convalidationDate = convalidationDate;
+    }
 }
