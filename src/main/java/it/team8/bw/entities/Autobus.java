e2753aff1956e7e5043e271f7f81650f63b2f580
Chiara Puleio <chiarapuleio@yahoo.it>
2024-01-29 14:31:45 +0100

    children classes

--- src/main/java/it/team8/bw/entities/Autobus.java
+++ src/main/java/it/team8/bw/entities/Autobus.java
@@ -1,4 +1,30 @@
 package it.team8.bw.entities;
 
-public class Autobus {
+import it.team8.bw.abstractClass.Means;
+import it.team8.bw.enums.MeansStatus;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@DiscriminatorValue("autobus")
+public class Autobus extends Means {
+
+    public Autobus(){}
+
+    public Autobus(MeansStatus meansStatus, Draft draft, Set<Maintenance> maintenance) {
+        super(50, meansStatus, draft, maintenance);
+    }
+
+    @Override
+    public String toString() {
+        return "Autobus{" +
+                "id=" + super.getId()+
+                ", capacity=" + capacity +
+                ", meansStatus=" + meansStatus +
+                ", obliterated=" + obliterated +
+                ", draft=" + draft +
+                ", maintenance=" + maintenance +
+                '}';
+    }
 }
--- src/main/java/it/team8/bw/entities/Tram.java
+++ src/main/java/it/team8/bw/entities/Tram.java
@@ -1,4 +1,29 @@
 package it.team8.bw.entities;
 
-public class Tram {
+import it.team8.bw.abstractClass.Means;
+import it.team8.bw.enums.MeansStatus;
+
+import javax.persistence.*;
+import java.util.Set;
+@Entity
+@DiscriminatorValue("tram")
+public class Tram extends Means {
+
+    public Tram(){}
+
+    public Tram(MeansStatus meansStatus, Draft draft, Set<Maintenance> maintenance) {
+        super(80, meansStatus, draft, maintenance);
+    }
+
+    @Override
+    public String toString() {
+        return "Tram{" +
+                "id=" + super.getId()+
+                ", capacity=" + capacity +
+                ", meansStatus=" + meansStatus +
+                ", obliterated=" + obliterated +
+                ", draft=" + draft +
+                ", maintenance=" + maintenance +
+                '}';
+    }
 }
