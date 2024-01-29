67f0f515b9fa9b14713b155168b8e73d63d23a1e
kavidiuk <kaviduwarnakulasuriya@gmail.com>
2024-01-29 15:02:21 +0100

    update user

--- src/main/java/it/team8/bw/entities/User.java
+++ src/main/java/it/team8/bw/entities/User.java
@@ -1,4 +1,42 @@
 package it.team8.bw.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
 public class User {
+ @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id",nullable = false)
+    private Long id;
+ protected String name;
+ protected String surname;
+
+    public User(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public User() {
+    }
 }
