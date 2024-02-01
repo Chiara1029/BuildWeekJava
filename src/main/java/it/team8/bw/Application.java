7dc4c0f81f5c784f51ac20abfd825a8c4f19a818
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2024-02-01 16:13:47 +0100

    add error db

--- src/main/java/it/team8/bw/Application.java
+++ src/main/java/it/team8/bw/Application.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestionetrasporti");
 
+    private static LogError log = new LogError();
 
     public static void main(String[] args) {
 
@@ -79,7 +80,7 @@ public class Application {
             ticketOfficeDAO.findById(input);
 
         } catch (Exception ex) {
-            LogError log = new LogError();
+
             log.setMessage(ex.getMessage());
             log.setTimestamp(new Date());
             logErrorDao.saveError(log);
